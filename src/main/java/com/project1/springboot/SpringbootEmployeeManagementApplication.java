69994964249e265c6cf65ecc024754bcc411b6d9
Ashish Kumar Singh <ashishsingh4job@gmail.com>
2023-01-16 01:59:27 +0530

    Add files via upload

---
+++ src/main/java/com/project1/springboot/SpringbootEmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.project1.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEmployeeManagementApplication.class, args);
+	}
+
+}
